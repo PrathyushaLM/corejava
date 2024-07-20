b10859b1c976074edd91cc1d128b476a21b7ff2b
karis <karis@LAPTOP-SJOF0NHK>
2024-07-20 20:14:25 +0530

    first commit

---
+++ corejava/src/com/tnsif/encapsulation/Encapsulation.java
@@ -0,0 +1,14 @@
+// program to demonstrate encapsulation
+package com.tnsif.encapsulation;
+
+public class Encapsulation {
+	int serialNum;// datavariables or datamembers
+	String name;
+	int age;
+	
+	void show() // member function
+	{
+		System.out.println("serialnumber"+serialNum+"\t Name"+name +"\t Age:"+ age);
+	}
+
+}
---
+++ corejava/src/com/tnsif/encapsulation/EncapsulationDemo.java
@@ -0,0 +1,14 @@
+package com.tnsif.encapsulation;
+
+public class EncapsulationDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Encapsulation ob= new Encapsulation();
+        ob.name="prathyu";//calling the variables
+        ob.age=21;
+        ob.serialNum=22;
+        ob.show();
+	}
+
+}
