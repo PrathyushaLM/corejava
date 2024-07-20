3872eee9e8f86e25467d0ccaa05e5df5efacacb9
karis <karis@LAPTOP-SJOF0NHK>
2024-07-20 23:59:54 +0530

    first commit

--- corejava/src/com/tnsif/encapsulation/Encapsulation.java
+++ corejava/src/com/tnsif/encapsulation/Encapsulation.java
@@ -2,13 +2,52 @@
 package com.tnsif.encapsulation;
 
 public class Encapsulation {
-	int serialNum;// datavariables or datamembers
-	String name;
-	int age;
+	private int serialNum;// datavariables or datamembers
+	private String name;
+	private int age;
 	
-	void show() // member function
-	{
-		System.out.println("serialnumber"+serialNum+"\t Name"+name +"\t Age:"+ age);
+	//by using getter and setter i can access private datamembers
+	
+	//to display string representation of the object
+	
+	@Override
+	public String toString() {
+		return "Encapsulation [serialNum=" + serialNum + ", name=" + name + ", age=" + age + "]";
 	}
 
+
+
+	public int getSerialNum() {   //
+		return serialNum;
+	}
+
+	public void setSerialNum(int serialNum) {
+		this.serialNum = serialNum;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	
+	
+	
+	
+	
+	
+
+	
+	
 }
--- corejava/src/com/tnsif/encapsulation/EncapsulationDemo.java
+++ corejava/src/com/tnsif/encapsulation/EncapsulationDemo.java
@@ -5,10 +5,15 @@ public class EncapsulationDemo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         Encapsulation ob= new Encapsulation();
-        ob.name="prathyu";//calling the variables
-        ob.age=21;
-        ob.serialNum=22;
-        ob.show();
+        
+    //    ob.name="prathyu";//calling the variables
+    //    ob.age=21;
+    //    ob.serialNum=22;
+        
+        ob.setAge(21);
+        ob.setName("kutti");
+        ob.setSerialNum(55);
+        System.out.println(ob);
 	}
 
 }
